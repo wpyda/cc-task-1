8978399ffd0084579aef9c67db7a2feabb233531
Wojtek Pyda <w.pyda@poczta.onet.pl>
2018-02-12 10:30:46 +0100

    long night, changed conception

--- Task 2/CCFrontTask2.js
+++ Task 2/CCFrontTask2.js
@@ -1,85 +1,88 @@
 const runes = [
-    {name: "El", power: 28},
-    {name: "Eld", power: 33},
-    {name: "Tir", power: 9},
-    {name: "Nef", power: 7},
-    {name: "Eth", power: 31},
-    {name: "Ith", power: 22},
-    {name: "Tal", power: 8},
-    {name: "Ral", power: 25},
-    {name: "Ort", power: 18},
-    {name: "Thul", power: 13},
-    {name: "Amn", power: 6},
-    {name: "Sol", power: 10},
-    {name: "Shael", power: 17},
-    {name: "Dol", power: 11},
-    {name: "Hel", power: 12},
-    {name: "Io", power: 20},
-    {name: "Lum", power: 32},
-    {name: "Ko", power: 27},
-    {name: "Fal", power: 14},
-    {name: "Lem", power: 26},
-    {name: "Pul", power: 15},
-    {name: "Um", power: 16},
-    {name: "Mal", power: 21},
-    {name: "Ist", power: 4},
-    {name: "Gul", power: 23},
-    {name: "Vex", power: 24},
-    {name: "Ohm", power: 1},
-    {name: "Lo", power: 2},
-    {name: "Sur", power: 30},
-    {name: "Ber", power: 3},
-    {name: "Jah", power: 5},
-    {name: "Cham", power: 29},
-    {name: "Zod", power: 19},
+    {name: "El", power: 28, dont: "Ort"},
+    {name: "Eld", power: 33, dont: "Sur"},
+    {name: "Tir", power: 9, dont: "Eth"},
+    {name: "Nef", power: 7, dont: "Ist"},
+    {name: "Eth", power: 31, dont: "Tir"},
+    {name: "Ith", power: 22, dont: "Pul"},
+    {name: "Tal", power: 8, dont: "Io"},
+    {name: "Ral", power: 25, dont: "Um"},
+    {name: "Ort", power: 18, dont: "El"},
+    {name: "Thul", power: 13, dont: "Sol"},
+    {name: "Amn", power: 6, dont: "Fal"},
+    {name: "Sol", power: 10, dont: "Thul"},
+    {name: "Shael", power: 17, dont: "Lem"},
+    {name: "Dol", power: 11, dont: "Hel"},
+    {name: "Hel", power: 12, dont: "Dol"},
+    {name: "Io", power: 20, dont: "Tal"},
+    {name: "Lum", power: 32, dont: "Gul"},
+    {name: "Ko", power: 27, dont: "Mal"},
+    {name: "Fal", power: 14, dont: "Amn"},
+    {name: "Lem", power: 26, dont: "Shall"},
+    {name: "Pul", power: 15, dont: "Ith"},
+    {name: "Um", power: 16, dont: "Ral"},
+    {name: "Mal", power: 21, dont: "Ko"},
+    {name: "Ist", power: 4, dont: "Nef"},
+    {name: "Gul", power: 23, dont: "Lum"},
+    {name: "Vex", power: 24, dont: "Ohm"},
+    {name: "Ohm", power: 1, dont: "Vex"},
+    {name: "Lo", power: 2, dont: "Cham"},
+    {name: "Sur", power: 30, dont: "Eld"},
+    {name: "Ber", power: 3, dont: ""},
+    {name: "Jah", power: 5, dont: "Zod"},
+    {name: "Cham", power: 29, dont: "Lo"},
+    {name: "Zod", power: 19, dont: "Jah"},
 ]
 
-exports.generateRunicWords = length => {
-
+const generateRunicWords = length => {
     if (typeof length !== "number") return Error("Input should be a number!")
     if (length === 0) return Error("Input should be greater than 0!")
+    if (length > runes.length) return Error("Number is too big! Not enough runes!")
 
-    const words = []
-    const newRunes = runes.map(el => el)
-    const runicWords = []
-
-    // A function which removes item from array
-    const removeRune = (array, element) => {
-        const index = array.indexOf(element)
-        if (index !== -1) {
-            array.splice(index, 1)
-        }
+    const findMostPowerfulRune = (inputRunes) => {
+        return inputRunes.reduce((reducer, el) => (
+            el.power > reducer.power ? el : reducer
+        ), {name: '', power: 0, dont: ''})
     }
 
-    // Pushing new keys and values to an empty array (words), so it contains segregated runes (from the greatest)
-    for (let i = 0; i < runes.length; i++) {
-        const rune = newRunes.reduce((max, rune) => (rune.power > max.power) ? rune : max)
-        words.push({name: rune.name, power: rune.power})
-        removeRune(newRunes, rune)
+    const generateRunicWord = (runesArr) => {
+        const runicWord = {name: '', power: 0}
+        runesArr.forEach((el, i, arr) => {
+            runicWord.name += el.name
+            runicWord.power += el.power
+            if (i < arr.length - 1) {
+                runicWord.name += '-'
+            }
+        })
+        return runicWord
     }
 
-    // Slicing words array and pushing it's elements to new empty array - runicWords
-    for (let i = 0; i < words.length; i += length) {
-        const slice = words.slice(i, i + length)
+    let inputRunes = [].concat(runes)
+    let runicWords = []
+    let maxPowerRune
+    let n = 0
 
-        if (slice.length === length) {
-            const name = slice.map(({name}) => name).join('-')
-            const power = slice.reduce((runicWords, {power}) => runicWords + power, 0)
-
-            runicWords.push({name, power})
+    // Generating runic words arrays 'length' times
+    do {
+        let runicWordArr = []
+        for (let i = 0; i < length; i++) {
+            maxPowerRune = findMostPowerfulRune(inputRunes)
+            inputRunes.splice(inputRunes.indexOf(maxPowerRune), 1)
+            runicWordArr[i] = maxPowerRune
         }
+        runicWords[n] = generateRunicWord(runicWordArr)
+        ++n
+    } while ((inputRunes.length > length) && (n < 10))
 
-        if (runicWords.length === 10) {
-            break
-        }
-    }
     return runicWords
 }
-//------TEST OUTPUT
-// console.log('generateRunicWords: ', generateRunicWords(5))
+
+//---- TEST OUTPUT
+console.log('generateRunicWords: ', generateRunicWords(5))
 
 
-exports.checkRunicWord = runicWord => {
+// A function which checks if typed runic word is valid
+const checkRunicWord = runicWord => {
     if (typeof runicWord !== 'string') return Error("Input is not a string!")
     if (runicWord === '') return Error("Input cannot be empty!")
 
@@ -98,4 +101,4 @@ exports.checkRunicWord = runicWord => {
 }
 
 //------TEST OUTPUT
-// console.log('checkRunicWord: ', checkRunicWord('Ohm-Lo-Ber-Ist'))
+console.log('checkRunicWord: ', checkRunicWord('Ohm-Lo-Ber-Ist'))
